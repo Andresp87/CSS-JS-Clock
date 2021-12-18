24b2d814361cc2993237cc83ff70c2983ce4fb7f
Andres Patino <andrespa87@gmail.com>
2021-12-18 08:05:57 -0500

    All the hands are working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const $seconds = document.querySelector('.seconds')
+const $minutes = document.querySelector('.minutes')
+const $hours = document.querySelector('.hours')
 
 const secondsInterval = setInterval(() => {
     const now = new Date()
@@ -7,4 +9,16 @@ const secondsInterval = setInterval(() => {
     $seconds.style.transform = `rotate(${seconds * DEGREE_ROTATION_PER_SECOND}deg)`;
 }, 1000)
 
+const minutesInterval = setInterval(() => {
+    const now = new Date();
+    const minutes = now.getMinutes();
+    const DEGREE_ROTATION_PER_MINUTE = 6;
+    $minutes.style.transform = `rotate(${minutes * DEGREE_ROTATION_PER_MINUTE}deg)`;
+}, 60000);
 
+const hoursInterval = setInterval(() => {
+    const now = new Date();
+    const hours = now.getHours();
+    const DEGREE_ROTATION_PER_HOUR = 30;
+    $hours.style.transform = `rotate(${hours * DEGREE_ROTATION_PER_HOUR}deg)`;
+}, 36000);
