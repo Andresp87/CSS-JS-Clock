3991066fca328ac48ea8c4da90d7f544b20bfaaa
Andres Patino <andrespa87@gmail.com>
2021-12-22 16:59:11 -0500

    seconds fixed and cleaning the code

--- src/index.js
+++ src/index.js
@@ -2,23 +2,19 @@ const $seconds = document.querySelector('.seconds')
 const $minutes = document.querySelector('.minutes')
 const $hours = document.querySelector('.hours')
 
+const DEGREE_ROTATION_PER_SECOND = 6
+const DEGREE_ROTATION_PER_MINUTE = 6;
+const DEGREE_ROTATION_PER_HOUR = 30;
+
+
 const secondsInterval = setInterval(() => {
-    const now = new Date()
+    const now = new Date();
+
     const seconds = now.getSeconds()
-    const DEGREE_ROTATION_PER_SECOND = 6
-    $seconds.style.transform = `rotate(${seconds * DEGREE_ROTATION_PER_SECOND}deg)`;
-}, 1000)
-
-const minutesInterval = setInterval(() => {
-    const now = new Date();
     const minutes = now.getMinutes();
-    const DEGREE_ROTATION_PER_MINUTE = 6;
-    $minutes.style.transform = `rotate(${minutes * DEGREE_ROTATION_PER_MINUTE}deg)`;
-}, 60000);
-
-const hoursInterval = setInterval(() => {
-    const now = new Date();
     const hours = now.getHours();
-    const DEGREE_ROTATION_PER_HOUR = 30;
+    
+    $seconds.style.transform = `rotate(${seconds * DEGREE_ROTATION_PER_SECOND}deg)`;
+    $minutes.style.transform = `rotate(${minutes * DEGREE_ROTATION_PER_MINUTE}deg)`;
     $hours.style.transform = `rotate(${hours * DEGREE_ROTATION_PER_HOUR}deg)`;
-}, 36000);
+}, 1000)
