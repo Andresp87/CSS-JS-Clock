fe91c6b8ba931d6849860ab91679e45d1368b381
Andres Patino <andrespa87@gmail.com>
2021-12-24 12:11:45 -0500

    code encapsulated in a function - project done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
-const $seconds = document.querySelector('.seconds')
-const $minutes = document.querySelector('.minutes')
-const $hours = document.querySelector('.hours')
-
-const DEGREE_ROTATION_PER_SECOND = 6
-const DEGREE_ROTATION_PER_MINUTE = 6;
-const DEGREE_ROTATION_PER_HOUR = 30;
+const clock = (() => {
+    const $seconds = document.querySelector('.seconds');
+    const $minutes = document.querySelector('.minutes');
+    const $hours = document.querySelector('.hours');
 
 
-const secondsInterval = setInterval(() => {
-    const now = new Date();
-
-    const seconds = now.getSeconds()
-    const minutes = now.getMinutes();
-    const hours = now.getHours();
+    if (!$seconds || !$minutes || !$hours) {
+        throw new Error ('No css elemnts found');
+    };
     
-    $seconds.style.transform = `rotate(${seconds * DEGREE_ROTATION_PER_SECOND}deg)`;
-    $minutes.style.transform = `rotate(${minutes * DEGREE_ROTATION_PER_MINUTE}deg)`;
-    $hours.style.transform = `rotate(${hours * DEGREE_ROTATION_PER_HOUR}deg)`;
-}, 1000)
+
+    const DEGREE_ROTATION = {
+        seconds: 6,
+        minutes: 6,
+        hours: 30
+    };
+    
+    
+    const clockInterval = setInterval(() => {
+        const now = new Date();
+    
+        const seconds = now.getSeconds();
+        const minutes = now.getMinutes();
+        const hours = now.getHours();
+        
+        $seconds.style.transform = `rotate(${seconds * DEGREE_ROTATION.seconds}deg)`;
+        $minutes.style.transform = `rotate(${minutes * DEGREE_ROTATION.minutes}deg)`;
+        $hours.style.transform = `rotate(${hours * DEGREE_ROTATION.hours}deg)`;
+    }, 1000);
+
+    return {
+        stop: () => clearInterval(clockInterval),
+    }
+});
+
+clock();
